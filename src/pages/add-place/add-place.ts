4b843f2f242d18725713188d2a9b54a20ee62745
林博斯 <lb0ss-Mac@LINBOSIdeMBP.T-mobile.com>
2018-10-17 22:28:02 -0700

    initial commit

--- src/pages/add-place/add-place.ts
+++ src/pages/add-place/add-place.ts
@@ -1,19 +1,17 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import { AddPlacePage } from '../add-place/add-place';
 
-@IonicPage()
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html',
+  selector: 'page-add-place',
+  templateUrl: 'add-place.html',
 })
-export class HomePage {
-  addPlacePage = AddPlacePage;
+export class AddPlacePage {
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad HomePage');
+    console.log('ionViewDidLoad AddPlacePage');
   }
 
 }
